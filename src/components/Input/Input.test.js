ddf002b43b6b7cf3db2f4e41c79b077aad7db259
Mazaher Muraj <mazaher.muraj@newcrosshealthcare.com>
2022-03-24 21:08:17 +0000

    writes tests for the input components

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,10 +1,56 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
-describe('<Input />', () => {
-  it('renders without crashing', () => {
-    render(<Input />);
-    const inputComponent = screen.getByTestId('todo-input');
-    expect(inputComponent).toBeInTheDocument();
+const setup = (state = {}) => {
+  const utils = render(<Input {...state}/>)
+  const input = utils.getByRole('textbox');
+  return {
+    input,
+    ...utils
+  }
+}
+
+describe('the input box', () => {
+  it('renders the component without errors', () => {
+    const { input } = setup();
+    expect(input).toBeInTheDocument();
   });
+  
+  it('allows me to type text into the input', () => {
+    const { input } = setup();
+    fireEvent.change(input, { target: { value: 'this is a todo' } });
+  
+    expect(input.value).toBe('this is a todo');
+  });
+  
+  it('displays a placeholder when there is a placeholder prop used', () => {
+    const { input } = setup({ placeholder: 'this is placeholder text'});
+  
+    expect(input).toHaveAttribute("placeholder", "this is placeholder text")
+  });
+
+  it('calls onKeyPress prop on press of Enter key', () => {
+    const onKeyPress = jest.fn();
+    const { input } = setup({ handleKeyPress: onKeyPress });
+
+    fireEvent.keyPress(input, { key: 'Enter', charCode: 13 });
+    expect(onKeyPress).toHaveBeenCalled();
+  })
+});
+
+describe('the submit button', () => {
+  it('renders the component without errors', () => {
+    const utils = render(<Input />)
+    const button = utils.getByRole('button');
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('calls onClick prop on button click', () => {
+    const onClick = jest.fn();
+    const { getByText } = setup({ buttonText: 'Click Me', handleSubmit: onClick })
+
+    fireEvent.click(getByText(/click me/i));
+    expect(onClick).toHaveBeenCalled();
+  })
 });
