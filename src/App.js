87fe1cec56820c73821d40fe116cdd650ffe1f3c
Mazaher Muraj <mazaher.muraj@newcrosshealthcare.com>
2022-03-24 21:07:48 +0000

    imports input component and creates handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import Input from './components/Input';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event) => setValue(event.target.value);
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+      setValue(event.target.value);
+    }
+  }
+
   return (
     <>
       <div>
         Hello World
       </div>
-      <Input />
+      <Input
+        placeholder='Enter a todo'
+        buttonText='+ Add Todo'
+        onChange={(event) => setValue(event.target.value)}
+        handleSubmit={handleSubmit}
+        handleKeyPress={handleKeyPress}
+        value={value}
+      />
     </>
   );
 }
