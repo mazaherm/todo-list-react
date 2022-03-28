7798c7b45f3884c9dfc6c5a250ea2a59ccca879d
Mazaher Muraj <mazaher.muraj@newcrosshealthcare.com>
2022-03-28 18:40:57 +0100

    creates and styles list item component

---
+++ src/components/List/ListItem/ListItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './ListItem.scss';
+
+const ListItem = ({
+  handleListIconClick,
+  listItemText,
+  fontAwesomeIcon
+}) => {
+  return (
+    <div className='ListItem' >
+      <li
+        className='ListItem__Item'
+      >
+          {listItemText}
+      </li>
+      <FontAwesomeIcon
+        icon={fontAwesomeIcon}
+        onClick={handleListIconClick}
+        title='list-icon'
+        className='ListItem__Icon'
+      />
+    </div>
+  );
+}
+
+export default ListItem
