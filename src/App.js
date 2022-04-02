5b3a1fe96ea9a94e28d0488d1cccabbeccada3f0
Mazaher Muraj <mazaher.muraj@newcrosshealthcare.com>
2022-04-02 01:24:06 +0100

    fixes bug in which same todo suggestion was being made + formats file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 
-import { fetchTodos, todosSelector } from './slices/todos';
+import { fetchTodos, todosSelector } from "./slices/todos";
 
-import Input from './components/Input';
-import Header from './components/Header';
-import List from './components/List';
+import Input from "./components/Input";
+import Header from "./components/Header";
+import List from "./components/List";
 
-import bannerImg from './assets/banner.jpg';
+import bannerImg from "./assets/banner.jpg";
 
-import './App.scss';
+import "./App.scss";
 
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-import rootReducer from './slices'
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import rootReducer from "./slices";
 
 // TODO: need to export this into its own file
 const AppWrapper = () => {
@@ -23,65 +23,58 @@ const AppWrapper = () => {
     <Provider store={store}>
       <App />
     </Provider>
-  )
-}
+  );
+};
 
 const App = () => {
   const dispatch = useDispatch();
   const { todos } = useSelector(todosSelector);
 
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [listItems, setListItems] = useState([]);
 
   useEffect(() => {
-    dispatch(fetchTodos())
-  }, [dispatch])
+    dispatch(fetchTodos());
+  }, [dispatch, todos.activity]);
 
   const handleSuggest = () => {
-    setValue(todos.activity)
-  }
+    setValue(todos.activity);
+  };
 
   const handleSubmit = (event) => {
-    setValue(event.target.value)
+    setValue(event.target.value);
     if (value.length) {
       listItems.push({
         todo: value,
-        id: uuidv4()
+        id: uuidv4(),
       });
     }
-    setListItems(listItems)
   };
 
   const handleXClick = () => {
-    setValue('');
-  }
+    setValue("");
+  };
 
   const handleListIconClick = (id) => {
-    const newItemsList =  listItems.filter(item => item.id !== id);
-    setListItems(newItemsList)
-  }
+    const newItemsList = listItems.filter((item) => item.id !== id);
+    setListItems(newItemsList);
+  };
 
   return (
     <>
-      <Header
-        bgImage={bannerImg}
-        heading='Todo List'
-      />
+      <Header bgImage={bannerImg} heading="Todo List" />
       <Input
-        placeholder='Enter a todo'
-        buttonText='+ Add Todo'
+        placeholder="Enter a todo"
+        buttonText="+ Add Todo"
         onChange={(event) => setValue(event.target.value)}
         handleSubmit={handleSubmit}
         handleSuggest={handleSuggest}
         handleXClick={handleXClick}
         value={value}
       />
-      <List
-        listItems={listItems}
-        onRemove={handleListIconClick}
-      />
+      <List listItems={listItems} onRemove={handleListIconClick} />
     </>
   );
-}
+};
 
 export default AppWrapper;
